100360a7a9b33d4ada8a304f0ee68f714fbc5f1b
tkachiko <tkachikos@gmail.com>
2022-09-20 21:59:37 +0300

    create Todolist component

---
+++ src/components/Todolist.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const Todolist = () => {
+  return (
+    <div>
+      <div>
+        <h3>What to learn</h3>
+        <input/>
+        <button>+</button>
+      </div>
+      <ul>
+        <li>
+          <input type='checkbox' checked={true}/>
+          <span>HTML&CSS</span>
+        </li>
+        <li>
+          <input type='checkbox' checked={true}/>
+          <span>JS</span>
+        </li>
+        <li>
+          <input type='checkbox' checked={false}/>
+          <span>React</span>
+        </li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+};
